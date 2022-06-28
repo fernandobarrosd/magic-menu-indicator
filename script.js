454be156270fc1706372be428fcefa5727f29014
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2022-06-27 21:14:26 -0300

    magic-menu-indicator update 4

--- script.js
+++ script.js
@@ -4,23 +4,22 @@ const menuItems = document.querySelectorAll(".menu-item");
 menuItems.forEach(menuItem => {
     const menuItemText = menuItem.querySelector
     (".menu-item-text");
-    const menuItemIcon = menuItem.querySelector
-    (".menu-item-icon");
-    
-    menuItemIcon.addEventListener("mouseenter", () => {
-       setAnimation(menuItemText, 0.5, 60, 1);
-       menuItemText.style.visibility = "visible";
+    const menuItemIconLink = menuItem.querySelector
+    (".menu-item-icon-link");
+
+    menuItemIconLink.addEventListener("mouseenter", () => {
+        setAnimation(menuItemText, 0.5, 60, 1, "visible");
     })
 
-    menuItemIcon.addEventListener("mouseout", () => {
-        setAnimation(menuItemText, 0.5, 80, 0);
-        menuItemText.style.visibility = "hidden";
+    menuItemIconLink.addEventListener("mouseout", () => {
+        setAnimation(menuItemText, 0.5, 80, 0, "hidden");
     })
 });
 
-function setAnimation(element, secondsValue, translateValue, opacityValue) {
+function setAnimation(element, secondsValue, translateValue, opacityValue, visibleValue) {
     element.style.transition = `${secondsValue}s`;
     element.style.transform = 
     `translate(${translateValue}px)`;
     element.style.opacity = opacityValue;
+    element.style.visibility = visibleValue
 }
